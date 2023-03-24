bc162674ca84ec0c6174f94988f9d8ee5c6be63c
jstonness1 <128709054+jstonness1@users.noreply.github.com>
2023-03-24 09:28:41 -0400

    first path

--- src/App.java
+++ src/App.java
@@ -1,14 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        clearscreen();
         System.out.println("\033[0;32m");
         System.out.println("welcome to the maze!");
-        
+      
+        System.out.println("There are 3 ways to go, ");
+        System.out.println("1, North");
+        System.out.println("2, East");
+        System.out.println("3, West");
+        System.out.println(" ");
+        int first = in.nextInt();
+
+        if (first == 1)
+            System.out.println("north");
+        if (first == 2)
+            System.out.println("east");
+        if (first >= 3)
+            System.out.println("west");
+       
+
+
+
+
+
+
+
+
+
+
     }
     
     /**
      * 
      */
-    public static void ClearScreen() {
+    public static void clearscreen() {
         System.out.print ("\033[H\033[2J");
     }
 
