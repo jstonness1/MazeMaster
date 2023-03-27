39dad92f97e9f983e8996c52915b085c83d83318
jstonness1 <128709054+jstonness1@users.noreply.github.com>
2023-03-27 09:14:34 -0400

    first directions

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         clearscreen();
@@ -12,14 +16,39 @@ public class App {
         System.out.println("2, East");
         System.out.println("3, West");
         System.out.println(" ");
-        int first = in.nextInt();
-
-        if (first == 1)
-            System.out.println("north");
-        if (first == 2)
-            System.out.println("east");
-        if (first >= 3)
-            System.out.println("west");
+        int start = in.nextInt();
+        String test=in.nextLine();
+        String firstWay;
+        if (start <= 1){
+            firstWay="youve chosen North now you have 4 more options";
+            System.out.println(" ");
+            System.out.println(firstWay);
+             System.out.println("1, North");
+         System.out.println("2, East");
+         System.out.println("3, West");
+         System.out.println("4, South");
+         System.out.println(" ");
+         int north1 = in.nextInt();
+        }
+        else if (start == 2){
+            firstWay="youve chosen East now you have 2 more options";
+            System.out.println(" ");
+            System.out.println(firstWay);
+             System.out.println("1, North");
+         System.out.println("2, West");
+         System.out.println(" ");
+         int east1 = in.nextInt();
+        }
+        else if (start >= 3){
+            firstWay="youve chosen West now you have 3 more options";
+           System.out.println(" ");
+           System.out.println(firstWay);
+            System.out.println("1, North");
+        System.out.println("2, East");
+        System.out.println("3, West");
+        System.out.println(" ");
+        int west1 = in.nextInt();
+        }
        
 
 
