9e0c08099414f57bdaa5413e816465935ae4f002
jstonness1 <128709054+jstonness1@users.noreply.github.com>
2023-03-30 09:02:31 -0400

    done

--- src/App.java
+++ src/App.java
@@ -14,7 +14,7 @@ public class App {
         int health = 10;
         String firstWay;
 
-        while (currentJunction != 5 && health >= 1) {
+        while (currentJunction != 6 && health >= 1) {
             if (currentJunction == 0) {
                 System.out.println("There are 3 ways to go, ");
                 System.out.println("1, North");
@@ -84,23 +84,61 @@ public class App {
                 }
             } else if (currentJunction == 1) {
 
+                firstWay = "youve chose West now you have 2 more options";
+                System.out.println(" ");
+                System.out.println(firstWay);
+                System.out.println("1, Final");
+                System.out.println("2, East");
+                System.out.println(" ");
+                int west1 = Integer.parseInt(in.nextLine());
             } else if (currentJunction == 2) {
                 firstWay = "youve chosen North now you have 2 more options";
                 System.out.println(" ");
                 System.out.println(firstWay);
-                System.out.println("1, West");
+                System.out.println("1, Final");
                 System.out.println("2, South");
                 System.out.println(" ");
 
                 int north1 = Integer.parseInt(in.nextLine());
-            }
+                currentJunction = 4;
+            } else if (currentJunction == 4) {
+                firstWay = "youve chosen Final now you have 1 way now";
+                System.out.println(" ");
+                System.out.println(firstWay);
+                System.out.println("1, ENDING");
+                System.out.println("");
+                System.out.println(" ");
+                int final1 = Integer.parseInt(in.nextLine());
+                if (final1 == 1) {
+                    currentJunction = 5;
+                }
+            } else if (currentJunction == 5) {
+                firstWay = "You've reached the ending";
+                String choice = "Would you like to start over? (Y/N)";
 
+                System.out.println(firstWay);
+                System.out.println(choice);
+                String final2 = in.nextLine();
+
+                if (final2.equalsIgnoreCase("y")) {
+                    System.out.println("You are being sent back to the start");
+                    clearscreen();
+                    currentJunction = 0;
+                }
+
+                else if (final2.equalsIgnoreCase("n")) {
+                    System.out.println("well played");
+
+                    currentJunction = 6;
+                }
+            }
         }
         if (health == 0) {
             System.out.println(" ");
             System.out.println("GAME OVER");
             System.out.println(" ");
         }
+
     }
 
     /**
